9f3c4f405800d450fd387c021a83f7f33b918179
abdulahadabduvahobov75@gmail.com <abdulahadabduvahobov75@gmail.com>
2023-11-11 17:09:30 +0500

    a

--- src/components/sertificate.vue
+++ src/components/sertificate.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="py-[50px] 2xl:py-[150px] overflow-x-hidden">
+    <div class="py-[50px] 2xl:py-[150px] overflow-hidden">
         <h1 class="text-center font-bold 2xl:text-[40px] 2xl:mb-[70px] text-[24px] mb-[30px]">Sertifikatlar</h1>
-        <div class="2xl:flex hidden items-center w-[1076px] mx-auto">
-            <button class="w-[353px] 2xl:w-[526px]">
-                <img src="../assets/sertificate1.svg" alt="sertificate">
-            </button>
-            <button class="w-[353px] 2xl:w-[526px]">
+        <div class="2xl:flex hidden items-center w-[1076px] mx-auto ">
+            <button class="w-[353px] 2xl:w-[526px] relative ">
                 <img src="../assets/sertificate2.svg" alt="sertificate">
+                <div class="absolute block inset-0 hover:bg-black w-[474px]  h-[583px] rounded-[4px] top-[21px] mx-auto hover:bg-opacity-[35%]">
+
+                </div>
+            </button>
+            <button class="w-[353px] 2xl:w-[526px] relative">
+                <img src="../assets/sertificate2.svg" alt="sertificate">
+                <div class="absolute block inset-0 hover:bg-black w-[474px]  h-[583px] rounded-[4px] top-[21px] mx-auto hover:bg-opacity-[35%]">
+
+                </div>  
             </button>
         </div>
     </div>
